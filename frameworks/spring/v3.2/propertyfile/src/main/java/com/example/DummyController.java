f64f0731abccaa4ea895f78350aad3f97de6cefc
Timo Friman <timo.friman@gmail.com>
2013-04-14 20:47:22 +0300

    Cleanup.

--- frameworks/spring/v3.2/propertyfile/src/main/java/com/example/DummyController.java
+++ frameworks/spring/v3.2/propertyfile/src/main/java/com/example/DummyController.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DummyController {
 
-    @Value("${example.bar:this is default value}")
+    @Value("${example.foo:this is default value}")
     private String aProperty;
-
-    @Autowired(required = false)
-    @Qualifier("exampleProperty")
-    private String anotherProperty = "default value";
 
     @RequestMapping("/")
     public
     @ResponseBody
     String handle() {
-        return aProperty + " and " + anotherProperty;
+        return aProperty;
     }
 }
